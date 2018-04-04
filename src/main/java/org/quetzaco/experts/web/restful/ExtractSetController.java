b4af7d90ae7341fc4b9938601b0a49e66e36fe24
Jill Yan <yan95599@gmail.com>
2018-04-04 17:36:36 +0800

    move json extract test to class Temp

--- src/main/java/org/quetzaco/experts/web/restful/ExtractSetController.java
+++ src/main/java/org/quetzaco/experts/web/restful/ExtractSetController.java
@@ -1,12 +1,7 @@
 package org.quetzaco.experts.web.restful;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.quetzaco.experts.app.biz.ExtractSetService;
 import org.quetzaco.experts.model.Udset;
-import org.quetzaco.experts.model.Udsetcompany;
 import org.quetzaco.experts.model.User;
 import org.quetzaco.experts.model.api.APIEntity;
 import org.quetzaco.experts.web.config.session.WebSecurityConfig;
@@ -20,39 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @RestController
 public class ExtractSetController extends BaseRestContoller {
 
     @Autowired
     ExtractSetService service;
-
-    public static void main(String args[]) {
-        Udset u = new Udset();
-        u.setProjectId(1);
-        List<Udsetcompany> cList = new ArrayList<Udsetcompany>();
-        Udsetcompany c = new Udsetcompany();
-        c.setProjectId(1);
-        cList.add(c);
-        u.setCompanyList(cList);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            String uJson = mapper.writeValueAsString(u);
-            System.out.println(uJson);
-            mapper.readValue(uJson, Udset.class);
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @RequestMapping(value = "/extractset/create", method = RequestMethod.POST)
     public HttpEntity<APIEntity> createExtractSet(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
