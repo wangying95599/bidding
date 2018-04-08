47a5ae436a43cfb53fd1115d5cd0f95f33001775
jillyan <zyan@talend.com>
2018-04-08 20:51:01 +0800

    expert create/update/delete

--- src/main/java/org/quetzaco/experts/web/restful/ExpertController.java
+++ src/main/java/org/quetzaco/experts/web/restful/ExpertController.java
@@ -25,57 +25,58 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @RestController
 public class ExpertController extends BaseRestContoller {
 
-    @Autowired
-    ExpertService expertService;
+	@Autowired
+	ExpertService expertService;
 
-    @RequestMapping(value = "/expert/search/{type}/{value}", method = RequestMethod.GET)
-    HttpEntity<APIEntity<List<Udexpert>>> getExpertList(@PathVariable String type, @PathVariable String value) {
+	@RequestMapping(value = "/expert/search/{type}/{value}", method = RequestMethod.GET)
+	HttpEntity<APIEntity<List<Udexpert>>> getExpertList(@PathVariable String type, @PathVariable String value) {
 
-        Udexpert expert = new Udexpert();
-        if ("name".equals(type)) {
-            expert.setName(value);
-        } else if ("phone".equals(type)) {
-            expert.setPhone(value);
-        } else if ("company".equals(type)) {
-            expert.setCompany(value);
-        }
+		Udexpert expert = new Udexpert();
+		if ("name".equals(type)) {
+			expert.setName(value);
+		} else if ("phone".equals(type)) {
+			expert.setPhone(value);
+		} else if ("company".equals(type)) {
+			expert.setCompany(value);
+		}
 
-        return buildEntity(APIEntity.create(expertService.selectByExample(expert)), HttpStatus.OK);
-    }
+		return buildEntity(APIEntity.create(expertService.selectByExample(expert)), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "expert/create", method = RequestMethod.POST)
-    public HttpEntity<APIEntity> create(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
-            @RequestBody Udprojects project) {
-        return buildEntity(APIEntity.create(null), HttpStatus.OK);
-    }
+	@RequestMapping(value = "expert", method = RequestMethod.POST)
+	public HttpEntity<APIEntity> create(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
+			@RequestBody Udexpert expert) {
+		Udexpert created = expertService.createExpert(expert);
+		return buildEntity(APIEntity.create(created), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "expert/all", method = RequestMethod.GET)
-    public HttpEntity<APIEntity> getAll(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user) {
-        List<Udexpert> all = expertService.selectByExample(null);
-        return buildEntity(APIEntity.create(all), HttpStatus.OK);
-    }
+	@RequestMapping(value = "expert/all", method = RequestMethod.GET)
+	public HttpEntity<APIEntity> getAll(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user) {
+		List<Udexpert> all = expertService.selectByExample(null);
+		return buildEntity(APIEntity.create(all), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "expert/{id}", method = RequestMethod.GET)
-    public HttpEntity<APIEntity> getProjectById(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
-            @RequestParam Integer id) {
-        Udexpert expert = expertService.getExpert(id);
-        return buildEntity(APIEntity.create(expert), HttpStatus.OK);
-    }
+	@RequestMapping(value = "expert/{id}", method = RequestMethod.GET)
+	public HttpEntity<APIEntity> getProjectById(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
+			@RequestParam Integer id) {
+		Udexpert expert = expertService.getExpert(id);
+		return buildEntity(APIEntity.create(expert), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "expert", method = RequestMethod.PUT)
-    public HttpEntity<APIEntity> updateProject(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
-            @RequestBody Udexpert expert) {
-        Udexpert updated = expertService.updateExpert(expert);
-        return buildEntity(APIEntity.create(updated), HttpStatus.OK);
-    }
+	@RequestMapping(value = "expert", method = RequestMethod.PUT)
+	public HttpEntity<APIEntity> updateProject(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
+			@RequestBody Udexpert expert) {
+		Udexpert updated = expertService.updateExpert(expert);
+		return buildEntity(APIEntity.create(updated), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "expert/delete", method = RequestMethod.DELETE)
-    public HttpEntity<APIEntity> deleteProjects(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
-            @RequestBody List<Integer> ids) {
-        for (Integer id : ids) {
-            expertService.deleteExpert(id);
-        }
-        return buildEntity(APIEntity.create(null), HttpStatus.OK);
-    }
+	@RequestMapping(value = "expert", method = RequestMethod.DELETE)
+	public HttpEntity<APIEntity> deleteProjects(@SessionAttribute(WebSecurityConfig.SESSION_KEY) User user,
+			@RequestBody List<Integer> ids) {
+		for (Integer id : ids) {
+			expertService.deleteExpert(id);
+		}
+		return buildEntity(APIEntity.create(null), HttpStatus.OK);
+	}
 
 }
--- src/main/resources/static/js/ud_expert.js
+++ src/main/resources/static/js/ud_expert.js
@@ -82,5 +82,78 @@ function setupExpertPage() {
             toggleExpertRelatedButton(selectedExpertNum);
         },
     });
+
+    $('#expertModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('title') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text(recipient);
+        let selectedExpert = $('#expert_table').bootstrapTable('getSelections')[0];
+        if (selectedExpert) {
+            console.log(selectedExpert);
+            modal.find('#expert_id_input').val(selectedExpert.expertId);
+            modal.find('#expert_no_input').val(selectedExpert.no);
+            modal.find('#expert_card_input').val(selectedExpert.card);
+            modal.find('#expert_name_input').val(selectedExpert.name);
+            modal.find('#expert_phone_input').val(selectedExpert.phone);
+            modal.find('#expert_company_input').val(selectedExpert.company);
+        }
+    })
+}
+
+function saveExpert() {
+    let expert = $('#expertForm').serializeJson();
+    if (expert.expertId) {//update
+        $.axx({
+            type: 'put',
+            url: '/expert',
+            contentType: 'application/json',
+            data: JSON.stringify(expert),
+            success: function (json) {
+                loadAllExperts();
+                $('#expertModal').modal('hide');
+            },
+            error: function (res) {
+                console.log(res);
+                alert(res);
+            }
+        });
+    } else {//create
+        $.axx({
+            type: 'post',
+            url: '/expert',
+            contentType: 'application/json',
+            data: JSON.stringify(expert),
+            success: function (json) {
+                loadAllExperts();
+                $('#expertModal').modal('hide');
+            },
+            error: function (res) {
+                console.log(res);
+                alert(res);
+            }
+        });
+    }
+}
+
+function deleteExperts() {
+    const ids = $('#expert_table').bootstrapTable('getSelections').map(function (item) {
+        return item.expertId;
+    });
+    $.axx({
+        type: 'delete',
+        url: '/expert',
+        contentType: 'application/json',
+        data: JSON.stringify(ids),
+        success: function (json) {
+            loadAllExperts();
+        },
+        error: function (res) {
+            console.log(res);
+            alert(res);
+        }
+    });
 }
 
