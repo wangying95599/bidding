49adfc48c748c899354cac22ab618deceb1e0e48
Jill Yan <yan95599@gmail.com>
2018-04-08 17:23:05 +0800

    add expert actions buttons
    Done:
    1. add delete button
    2. toggle create/edit/delete
    3. handle null condition in select expert service
    
    Not done:
    expert modal

--- src/main/resources/static/js/ud_expert.js
+++ src/main/resources/static/js/ud_expert.js
@@ -1,9 +1,12 @@
+let selectedExpertNum = 0, expertTotal = 0;
 const expert_table_cols = [{
+    checkbox: true,
+}, {
     field: 'no',
     title: '专家编号'
 }, {
     field: 'card',
-    title: ''
+    title: 'card'
 }, {
     field: 'name',
     title: '姓名'
@@ -18,14 +21,15 @@ const expert_table_cols = [{
     title: '所在区域'
 }, {
     field: 'record_flag',
-    title: ''
+    title: 'record_flag'
 }];
 
 function loadAllExperts() {
     $.axx({
         type: 'get',
-        url: '/experts/all',
+        url: '/expert/all',
         success: function (json) {
+            expertTotal = json.content.length;
             $('#expert_table').bootstrapTable("load", json.content);
         },
         error: function (res) {
@@ -35,10 +39,26 @@ function loadAllExperts() {
     })
 }
 
-
 function showExpertTab() {
     $('#expertMaintenanceTab').tab('show');
     loadAllExperts();
+}
+
+function toggleExpertRelatedButton(selectedNum) {
+
+    if (selectedNum === 0) {
+        $('#createExpertButton').show();
+        $('#editExpertButton').hide();
+        $('#deleteExpertButton').hide();
+    } else if (selectedNum === 1) {
+        $('#createExpertButton').hide();
+        $('#editExpertButton').show();
+        $('#deleteExpertButton').show();
+    } else if (selectedNum > 1) {
+        $('#createExpertButton').hide();
+        $('#editExpertButton').hide();
+        $('#deleteExpertButton').show();
+    }
 }
 
 function setupExpertPage() {
@@ -46,6 +66,22 @@ function setupExpertPage() {
         columns: expert_table_cols,
         pagination: false,
         clickToSelect: true,
+        onCheck: function () {
+            selectedExpertNum++;
+            toggleExpertRelatedButton(selectedExpertNum);
+        },
+        onUncheck: function () {
+            selectedExpertNum--;
+            toggleExpertRelatedButton(selectedExpertNum);
+        },
+        onCheckAll: function () {
+            selectedExpertNum = expertTotal;
+            toggleExpertRelatedButton(selectedExpertNum);
+        },
+        onUncheckAll: function () {
+            selectedExpertNum = 0;
+            toggleExpertRelatedButton(selectedExpertNum);
+        },
     });
 }
 
