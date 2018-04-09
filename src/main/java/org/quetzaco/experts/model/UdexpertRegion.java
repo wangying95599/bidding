59fdd36416c56400506157501f9e2c57b8a58e83
jillyan <zyan@talend.com>
2018-04-09 22:17:14 +0800

    add expert_region table and related java

---
+++ src/main/java/org/quetzaco/experts/model/UdexpertRegion.java
@@ -0,0 +1,35 @@
+package org.quetzaco.experts.model;
+
+import java.util.Date;
+
+public class UdexpertRegion {
+    private Integer expertId;
+
+    private String region;
+
+    private Date createdDt;
+
+    public Integer getExpertId() {
+        return expertId;
+    }
+
+    public void setExpertId(Integer expertId) {
+        this.expertId = expertId;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region == null ? null : region.trim();
+    }
+
+    public Date getCreatedDt() {
+        return createdDt;
+    }
+
+    public void setCreatedDt(Date createdDt) {
+        this.createdDt = createdDt;
+    }
+}
