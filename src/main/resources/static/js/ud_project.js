1e4dc20b8250588c2015fdd405bb3720ec655aa4
jillyan <zyan@talend.com>
2018-04-13 20:59:51 +0800

    refactor

--- src/main/resources/static/js/ud_project.js
+++ src/main/resources/static/js/ud_project.js
@@ -177,6 +177,18 @@ function setUpProjectPage() {
             setModalData(modal, project_modal_mapper);
         }
     });
+    $(function () {
+        $('#datetimepicker1').datetimepicker({
+            autoclose: true,
+            todayBtn: true,
+            todayHighlight: true,
+            showMeridian: true,
+            pickerPosition: "bottom-left",
+            language: 'zh-CN',//中文，需要引用zh-CN.js包
+            startView: 0,//月视图
+            minView: 0//日期时间选择器所能够提供的最精确的时间选择视图
+        });
+    });
 }
 
 function createProject() {
