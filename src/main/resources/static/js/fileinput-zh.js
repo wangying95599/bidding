3a29d0a0efeb1fa7bc414425bb4d674fcca461ef
wangying95599 <528142048@qq.com>
2018-04-14 11:44:50 +0800

    目前的场景都是单文件上传

--- src/main/resources/static/js/fileinput-zh.js
+++ src/main/resources/static/js/fileinput-zh.js
@@ -73,7 +73,7 @@
             uploadBatch: '批量上传',
             uploadExtra: '表单数据上传'
         },
-        dropZoneTitle: '拖拽文件到这里 &hellip;<br>支持多文件同时上传',
+        dropZoneTitle: '拖拽文件到这里 &hellip;<br>',
         dropZoneClickTitle: '<br>(或点击{files}按钮选择文件)',
         fileActionSettings: {
             removeTitle: '删除文件',
