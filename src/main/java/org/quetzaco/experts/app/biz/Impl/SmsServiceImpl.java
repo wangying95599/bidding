4dee95d5bd38004dafd6d03a4bc889647659b23c
wangying95599 <wangying95599@163.com>
2018-04-18 12:37:04 +0800

    短信处理通知回馈

--- src/main/java/org/quetzaco/experts/app/biz/Impl/SmsServiceImpl.java
+++ src/main/java/org/quetzaco/experts/app/biz/Impl/SmsServiceImpl.java
@@ -1,5 +1,7 @@
 package org.quetzaco.experts.app.biz.Impl;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,9 +9,11 @@ import java.util.Map;
 import org.quetzaco.experts.app.biz.SmsService;
 import org.quetzaco.experts.app.dao.UdprojectsMapper;
 import org.quetzaco.experts.app.dao.UdsetresultMapper;
+import org.quetzaco.experts.enums.ProjectStatus;
 import org.quetzaco.experts.model.Udprojects;
 import org.quetzaco.experts.model.Udsetresult;
 import org.quetzaco.experts.model.UdsetresultExample;
+import org.quetzaco.experts.model.Udvoicelog;
 import org.quetzaco.experts.util.json.JsonUtil;
 import org.quetzaco.experts.util.notify.sms.SmsNotify;
 import org.slf4j.Logger;
@@ -56,8 +60,95 @@ public class SmsServiceImpl implements SmsService {
 
 	@Override
 	public void getSms(Integer projectId) {
-		
+		// getSmsLog 还是udsetresult
+	}
+	
+	public void insertSmslog(Udvoicelog log) {
+		try {
+			String outId=log.getOutId();
+			String[] strs = outId.split("===");
+			log.setProjectId(Integer.parseInt(strs[0]));
+			log.setExpertId(Integer.parseInt(strs[1]));
+			log.setRandomCode(strs[2]);
+			
+			//TODO
+			//smslogMapper.insertSelective(log);
+			
+			Udsetresult result = new Udsetresult ();
+			result.setProjectId(log.getProjectId());
+			result.setExpertId(log.getExpertId());
+			
+			//不接，接了不按，1,2，占线。
+			if("1".equals(log.getDtmf())) {
+				confirm(result, true);
+			}else {
+				confirm(result, false);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	
+	public void confirm(Udsetresult result, boolean isAgree) {
+		Udsetresult record = new Udsetresult();
+		if(isAgree) {
+			record.setConfirmStatus("Y");
+		}else {
+			record.setConfirmStatus("N");
+		}
+		
+		
+		UdsetresultExample example = new UdsetresultExample ();
+		example.createCriteria().andProjectIdEqualTo(result.getProjectId());
+		example.getOredCriteria().get(0).andExpertIdEqualTo(result.getExpertId());
+		List<Udsetresult> list = resultMapper.selectByExample(example);
+		if(list!=null && list.size()>0) {
+			Integer notifyNumber = list.get(0).getNotifyNumber()==null?0:list.get(0).getNotifyNumber();
+			record.setNotifyNumber(notifyNumber+1);
+			resultMapper.updateByExampleSelective(record, example);
+		}
+		
+		updateProjectStatus(result.getProjectId());
+	}
+	
+	/**
+	 * 一般就是5个人
+	 * @param projectId
+	 */
+	private void updateProjectStatus(Integer projectId) {
+		UdsetresultExample example = new UdsetresultExample ();
+		example.createCriteria().andProjectIdEqualTo(projectId);
+		example.createCriteria().andConfirmStatusEqualTo("Y");
+//		example.getOredCriteria().get(0).and
+		long confirmNumber = resultMapper.countByExample(example);
+		if(confirmNumber == 5) {
+			//TODO
+			Udprojects project =new Udprojects();
+			project.setId(projectId);
+			project.setProjectStatus(ProjectStatus.CONFIRMED.getValue());
+			projectMapper.updateByPrimaryKeySelective(project);
+		}else {
+			//TODO 状态为已通知
+			Udprojects project =new Udprojects();
+			project.setId(projectId);
+			project.setProjectStatus(ProjectStatus.CONFIRM.getValue());
+			projectMapper.updateByPrimaryKeySelective(project);
+		}		
+		
+	}
+	
+	/**
+	 * 这样就不要定时几次了，让用户手动点击吧
+	 */
+	//TODO
+	//@Scheduled(cron = "0 * * * * *")  
+	public void timer(){  
+	    //获取当前时间  
+	    LocalDateTime localDateTime =LocalDateTime.now();  
+	    System.out.println("当前时间为:" + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));  
+	    
+	}  
 }
--- src/main/java/org/quetzaco/experts/app/biz/SmsService.java
+++ src/main/java/org/quetzaco/experts/app/biz/SmsService.java
@@ -1,7 +1,12 @@
 package org.quetzaco.experts.app.biz;
 
+import org.quetzaco.experts.model.Udsetresult;
+import org.quetzaco.experts.model.Udvoicelog;
+
 public interface SmsService {
 	public void startSms(Integer id) ;
 	public void getSms(Integer id) ;
+	public void insertSmslog(Udvoicelog log);
 	
+	public void confirm(Udsetresult result, boolean isAgree);
 }
